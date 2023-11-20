833beff1d706e314aeab4a153ad4c286d93e7757
danielplebani <danielpleba03@gmail.com>
2023-11-20 17:51:22 +0100

    add the single page for the project

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController;
-use App\Http\Controllers\Api\TechnologyController;
-use App\Http\Controllers\Api\TypeController;
-
 
 /*
 |--------------------------------------------------------------------------
